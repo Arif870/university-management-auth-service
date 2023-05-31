2a6373d9af7e5e3c08f95e2fa94cb92f33c2b7da
Arif uz-zaman <arif.uz-zaman@roche.com>
2023-05-31 10:49:45 +0600

    [done] eslint, prettier, husky and lint stage

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import express, { Application, NextFunction, Request, Response } from 'express'
+import express, { Application, Request, Response } from 'express'
 const app: Application = express()
 import cors from 'cors'
 
 // parse
-          app.use(cors())
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 
 // Testing route
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Working succsessfully')
 })
 
